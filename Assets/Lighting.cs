1cffa3f1a6b016bee90e391b534a558b37ec6cc5
Dorian Boché <dorian.boche@outlook.fr>
2018-02-14 10:21:01 -0500

    add pre test and lighting

---
+++ Assets/Lighting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lighting : MonoBehaviour
+{
+
+    public Light light;
+    private float forcelight;
+    private Flash flash;
+    // Use this for initialization
+    void Start()
+    {
+        forcelight = 0;
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (forcelight < 1.5)
+        {
+            light.intensity = forcelight;
+            forcelight += Time.deltaTime;
+        }
+        else
+        {
+            //flash.Blink();
+        }
+    }
+}
---
+++ Assets/Scripts/Flash.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flash : MonoBehaviour {
+
+    public Light light;
+    public float blinkingTime = 0.5f;
+    public float offTime = 0.5f;
+    private float timeOn;
+    private float timeOff;
+    private bool on = false;
+    private bool off = false;
+
+    void Start()
+    {
+        timeOn = 0.0f;
+        timeOff = 0.0f;
+        light = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        Blink();
+    }
+
+    public void Blink()
+    {
+        if (timeOn < blinkingTime)
+        {
+            light.intensity = 1;
+            timeOn += Time.deltaTime;
+            Debug.Log(timeOn);
+        }
+
+        else
+        {
+            on = true;
+            if (timeOff < offTime)
+            {
+                light.intensity = 0;
+                timeOff += Time.deltaTime;
+            }
+            else
+            {
+                off = true;
+            }
+        }
+
+        if (on == true && off == true)
+        {
+            timeOn = 0.0f;
+            timeOff = 0.0f;
+            on = false;
+            off = false;
+        }
+    }
+
+ 
+}
