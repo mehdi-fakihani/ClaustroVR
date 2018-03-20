527df0d59db70be6dd47e56396ad35f2424654fd
Natacha Guillet <guillet.natacha@outlook.fr>
2018-03-19 22:41:48 -0400

    update player

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -2,45 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GamepadManager : MonoBehaviour
-{
+public class CameraManager : MonoBehaviour {
+
     public float speed = 6f;            // The speed that the player will move at.
 
     Vector3 movement;                   // The vector to store the direction of the player's movement.
-    Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
-    int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
-    float camRayLength = 100f;          // The length of the ray from the camera into the scene.
 
     void Awake()
     {
-        // Create a layer mask for the floor layer.
-        floorMask = LayerMask.GetMask("Floor");
 
-        // Set up references.
-        playerRigidbody = GetComponent<Rigidbody>();
     }
-
 
     void FixedUpdate()
     {
         // Store the input axes.
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("CameraHorizontal");
+        float v = Input.GetAxisRaw("CameraVertical");
 
         // Move the player around the scene.
+        Debug.Log(v);
+        Debug.Log(h);
         Move(h, v);
-
     }
 
     void Move(float h, float v)
     {
         // Set the movement vector based on the axis input.
-        movement.Set(h, 0f, v);
+        movement.Set(v, h, 0f);
 
         // Normalise the movement vector and make it proportional to the speed per second.
         movement = movement.normalized * speed * Time.deltaTime;
 
+        this.transform.Rotate(movement);
         // Move the player to it's current position plus the movement.
-        playerRigidbody.MovePosition(transform.position + movement);
+        //playerRigidbody.MovePosition(transform.position + movement);
     }
 }
