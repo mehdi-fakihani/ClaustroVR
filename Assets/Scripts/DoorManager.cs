ddf7833a2d87f515e78d64ae5be23b9055279cdf
mehdi-fakihani <mehdi.fakihani@outlook.com>
2018-03-21 12:08:35 -0400

    The door can now be opened

---
+++ Assets/Scripts/DoorManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+
+    private static float doorAngularSpeed = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            StartCoroutine(openDoor());
+    }
+
+    IEnumerator openDoor()
+    {
+        float step = doorAngularSpeed * Time.deltaTime;
+        Quaternion target = Quaternion.Euler(0, 90, 0);
+        while (transform.rotation != target)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, target, step);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
