57cdb3b93814a3b270646118282f55f166fb243f
Natacha Guillet <guillet.natacha@outlook.fr>
2018-03-22 02:13:52 -0400

    add fade

---
+++ Assets/Scripts/SteamVrFadeTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteamVrFadeTest : MonoBehaviour {
+
+	private float _fadeDuration = 2f;
+
+	private void Start()
+	{
+		FadeToBlack();
+		Invoke("FadeFromWhite", _fadeDuration);
+	}
+	private void FadeToBlack()
+	{
+		//set start color
+		SteamVR_Fade.Start(Color.clear, 0f);
+		//set and start fade to
+		SteamVR_Fade.Start(Color.black, _fadeDuration);
+	}
+	private void FadeFromWhite()
+	{
+		//set start color
+		SteamVR_Fade.Start(Color.white, 0f);
+		//set and start fade to
+		SteamVR_Fade.Start(Color.clear, _fadeDuration);
+	}
