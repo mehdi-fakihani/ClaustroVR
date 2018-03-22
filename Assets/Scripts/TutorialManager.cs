f9ba0d8b18dd1e6ca1db0e8eafe508da8342c466
Natacha Guillet <guillet.natacha@outlook.fr>
2018-03-22 01:34:16 -0400

    added video and cards

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour {
+
+	[SerializeField] private GameObject tutoMove;
+	[SerializeField] private GameObject tutoButton;
+	[SerializeField] private GameObject tutoEnd;
+
+	private bool move = false;
+	private bool button = false;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (move && !button) {
+			tutoMove.SetActive (false);
+			tutoButton.SetActive (true);
+		} else if (move && button) {
+			tutoButton.SetActive (false);
+			tutoEnd.SetActive (true);
+		}
+	}
+}
